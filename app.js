6143eed8b77e550b5156bcfdfb293d3c34b8f6aa
opcja <piotrmajda7@gmail.com>
2021-02-19 14:37:27 +0100

    work on navigation

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-//var createError = require('http-errors');
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 const fs = require('fs');
-//var cookieParser = require('cookie-parser');
-//var logger = require('morgan');
-//var debug = require('debug')('test:server');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var debug = require('debug')('test:server');
 var http = require('http');
 
 var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+console.log(__dirname);
 
-//app.use(logger('dev'));
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) { // ogarnia aktualny path w routingu
@@ -29,23 +29,21 @@ app.use(function(req, res, next) { // ogarnia aktualny path w routingu
 });
 
 app.use('/', indexRouter);
-//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+ app.use(function(req, res, next) {
+   next(createError(404));
+ });
 
 // // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+ app.use(function(err, req, res, next) {
+   // set locals, only providing error in development
+   res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+   // render the error page
+   res.status(err.status || 500);
+   res.render('error');
+ });
 
 var port = (process.env.PORT || '3000');
 app.set('port', port);
@@ -57,66 +55,66 @@ var server = http.createServer(app);
  */
 
 server.listen(port);
-//server.on('error', onError);
-//server.on('listening', onListening);
+server.on('error', onError);
+server.on('listening', onListening);
 
 /**
  * Normalize a port into a number, string, or false.
  */
 
-// function normalizePort(val) {
-//   var port = parseInt(val, 10);
+ function normalizePort(val) {
+   var port = parseInt(val, 10);
 
-//   if (isNaN(port)) {
-//     // named pipe
-//     return val;
-//   }
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
 
-//   if (port >= 0) {
-//     // port number
-//     return port;
-//   }
+  if (port >= 0) {
+    // port number
+    return port;
+  }
 
-//   return false;
-// }
+  return false;
+}
 
-// /**
-//  * Event listener for HTTP server "error" event.
-//  */
+/**
+ * Event listener for HTTP server "error" event.
+ */
 
-// function onError(error) {
-//   if (error.syscall !== 'listen') {
-//     throw error;
-//   }
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
 
-//   var bind = typeof port === 'string'
-//     ? 'Pipe ' + port
-//     : 'Port ' + port;
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
 
-//   // handle specific listen errors with friendly messages
-//   switch (error.code) {
-//     case 'EACCES':
-//       console.error(bind + ' requires elevated privileges');
-//       process.exit(1);
-//       break;
-//     case 'EADDRINUSE':
-//       console.error(bind + ' is already in use');
-//       process.exit(1);
-//       break;
-//     default:
-//       throw error;
-//   }
-// }
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
 
-// /**
-//  * Event listener for HTTP server "listening" event.
-//  */
+/**
+ * Event listener for HTTP server "listening" event.
+ */
 
-// function onListening() {
-//   var addr = server.address();
-//   var bind = typeof addr === 'string'
-//     ? 'pipe ' + addr
-//     : 'port ' + addr.port;
-//   debug('Listening on ' + bind);
-// }
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
 
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
-        title: 'Uszy'
+        title: 'Uszy do góry'
     });
 });
 
